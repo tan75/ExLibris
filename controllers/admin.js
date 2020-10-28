3ebb6bfe01371091881a85188b2bbbb6a4128ffb
Tetiana <ttobin@protonmail.ch>
2020-10-28 21:03:16 +0000

    Editing Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -24,52 +24,56 @@ exports.postAddBook = (req, res) => {
     .catch((err) => console.log(err));
 };
 
-// exports.getEditBook = (req, res) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect("/");
-//   }
+exports.getEditBook = (req, res) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
 
-//   const bookId = req.params.bookId;
-//   Book.findById(bookId, (book) => {
-//     if (!book) {
-//       return res.redirect("/");
-//     }
-//     res.render("admin/edit-book", {
-//       pageTitle: "Edit Book",
-//       path: "/admin/edit-book",
-//       editing: editMode,
-//       book: book,
-//     });
-//   });
-// };
+  const bookId = req.params.bookId;
+  Book.findById(bookId)
+    .then((book) => {
+      if (!book) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-book", {
+        pageTitle: "Edit Book",
+        path: "/admin/edit-book",
+        editing: editMode,
+        book: book,
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
-// exports.postEditBook = (req, res) => {
-//   const bookId = req.body.bookId;
-//   const updatedTitle = req.body.title;
-//   const updatedPages = req.body.pages;
-//   const updatedImageUrl = req.body.imageUrl;
-//   const updatedDescription = req.body.description;
-//   const updatedBook = new Book(
-//     bookId,
-//     updatedTitle,
-//     updatedPages,
-//     updatedImageUrl,
-//     updatedDescription
-//   );
-//   updatedBook.save();
-//   res.redirect("/admin/books");
-// };
+exports.postEditBook = (req, res) => {
+  const bookId = req.body.bookId;
+  const updatedTitle = req.body.title;
+  const updatedPages = req.body.pages;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  const updatedBook = new Book(
+    bookId,
+    updatedTitle,
+    updatedPages,
+    updatedImageUrl,
+    updatedDescription
+  );
+  updatedBook.save();
+  res.redirect("/admin/books");
+};
 
-// exports.getBooks = (req, res) => {
-//   Book.fetchAll((books) => {
-//     res.render("admin/books", {
-//       bks: books,
-//       pageTitle: "Admin Books",
-//       path: "/admin/books",
-//     });
-//   });
-// };
+exports.getBooks = (req, res) => {
+  Book.fetchAll()
+    .then((books) => {
+      res.render("admin/books", {
+        bks: books,
+        pageTitle: "Admin Books",
+        path: "/admin/books",
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
 // exports.postDeleleBook = (req, res) => {
 //   const bookId = req.body.bookId;
