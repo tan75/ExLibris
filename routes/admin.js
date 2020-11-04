d28d4cba1b416db9b2cfd01f7d21ab333962f73a
Tetiana <ttobin@protonmail.ch>
2020-11-04 21:02:26 +0000

    Validation fixes
    -- User Input saved for Edit Book
    -- Margin added to book-item

--- routes/admin.js
+++ routes/admin.js
@@ -16,7 +16,7 @@ router.post(
   check("title")
     .isAlphanumeric()
     .withMessage("Please Enter Alphanumeric Characters Only"),
-  check("imageUrl").isURL().withMessage("Please Enter Valid Email"),
+  check("imageUrl").isURL().withMessage("Please Enter Valid URL"),
   check("pages").isInt().withMessage("Please Enter Valid Number of Pages"),
   check("description")
     .isAlphanumeric()
@@ -26,7 +26,22 @@ router.post(
 
 router.get("/edit-book/:bookId", adminController.getEditBook);
 
-router.post("/edit-book", adminController.postEditBook);
+router.post(
+  "/edit-book",
+  [
+    check("title")
+      //.isLength({ max: 50 })
+      .isAlphanumeric()
+      .withMessage("Please Enter Alphanumeric Characters Only"),
+    check("imageUrl").isURL().withMessage("Please Enter Valid URL"),
+    check("pages").isInt().withMessage("Please Enter Valid Number of Pages"),
+    check("description")
+      .isLength({ max: 500 })
+      .isAlphanumeric()
+      .withMessage("Please Enter Valid Description"),
+  ],
+  adminController.postEditBook
+);
 
 router.post("/delete-book", adminController.postDeleleBook);
 
