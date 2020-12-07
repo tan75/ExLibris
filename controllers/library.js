ca1fee588dc0ab5a715965f55d6337d2aa7c9b73
Tetiana <ttobin@protonmail.ch>
2020-12-07 09:21:28 +0000

    Update library.js

--- controllers/library.js
+++ controllers/library.js
@@ -1,9 +1,15 @@
 const Book = require("../models/book");
+const mongoDb = require("mongodb");
 
 exports.getBooks = (req, res) => {
   Book.fetchAll()
     .then((books) => {
-      res.json({books});
+      //res.status(200).json({ books });
+      res.render("library/book-list", {
+        bks: books,
+        pageTitle: "All Books",
+        path: "/books",
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -13,7 +19,8 @@ exports.getBook = (req, res) => {
   const bookId = req.params.bookId; // comes from the route
   Book.findById(bookId)
     .then((book) => {
-      res.json({book})
+      console.log(book);
+      res.status(200).json({book})
     })
     .catch((err) => console.log(err));
 };
@@ -21,21 +28,17 @@ exports.getBook = (req, res) => {
 exports.getIndex = (req, res) => {
   Book.fetchAll()
     .then((books) => {
-      res.json({books});
+      res.status(200).json({books});
     })
     .catch((err) => console.log(err));
 };
 
+// http://localhost:8000/report
 exports.getReport = (req, res) => {
   req.user
     .getReport()
     .then((reportBooks) => {
-      res.render("library/report", {
-        path: "/report",
-        pageTitle: "Your Reading Report",
-        books: reportBooks,
-        totalPages: req.user.report.totalPages,
-      });
+      res.status(200).json({reportBooks});
     })
     .catch((err) => console.log(err));
 };
@@ -48,7 +51,10 @@ exports.postReport = (req, res) => {
     })
     .then((result) => {
       console.log(result);
-      res.redirect("/report");
+      res.redirect("/report")
+      // res.json({
+      //   message: 'Book Added to the Report',
+      //   book: {_id: new mongoDb.ObjectID(bookId), title: req.bod.title, pages: req.body.pages}  })       
+        .catch((err) => console.log(err));
     })
-    .catch((err) => console.log(err));
 };
